33bad7be335b228214b3c1fa4aea12d7548e89a5
paczaj <marcinpaczkowski@icloud.com>
2025-07-06 14:35:17 +0200

    Import AuthModule into UserModule to enable authentication dependencies

--- src/api/user/user.module.ts
+++ src/api/user/user.module.ts
@@ -3,10 +3,11 @@ import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from '../../entities/user.entity';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
   controllers: [UserController],
   providers: [UserService],
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User]), AuthModule],
 })
 export class UserModule {}
