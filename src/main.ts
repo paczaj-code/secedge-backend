20aa5da5f1105c2b280a4c3ba3e6aacd52be71ae
paczaj <marcinpaczkowski@icloud.com>
2025-07-06 07:16:10 +0200

    Enhance application with global prefix, CORS configuration, and compression middleware.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as compression from 'compression';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
+
+  app.use(compression());
+
+  app.enableCors({
+    allowedHeaders: '*',
+    origin: '*',
+    credentials: true,
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
