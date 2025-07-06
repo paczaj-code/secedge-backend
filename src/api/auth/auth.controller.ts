f7bbceef912e033a37375d10e17010731a83b5dd
paczaj <marcinpaczkowski@icloud.com>
2025-07-06 12:11:24 +0200

    Enhance AuthController with detailed JSDoc comments and update login and refreshToken methods to return tokens

--- src/api/auth/auth.controller.ts
+++ src/api/auth/auth.controller.ts
@@ -10,19 +10,40 @@ import {
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
 
+/**
+ * Controller responsible for handling authentication-related requests.
+ */
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  /**
+   * Handles user login by validating credentials and providing access and refresh tokens.
+   *
+   * @param {LoginDto} credentials - Contains the properties required for user authentication, such as email and password.
+   * @return {Promise<{ refreshToken: string; accessToken: string }>} A promise that resolves to an object containing the refresh token and access token for the authenticated user.
+   */
   @HttpCode(HttpStatus.OK)
   @Post('login')
-  login(@Body() credentials: LoginDto) {
+  async login(
+    @Body() credentials: LoginDto,
+  ): Promise<{ refreshToken: string; accessToken: string }> {
     return this.authService.login(credentials);
   }
 
+  /**
+   * Handles the refresh token functionality. Extracts the token from the request headers,
+   * validates it, and calls the corresponding service to generate a new token.
+   *
+   * @param {Request} request - The HTTP request object containing the authorization header with the token.
+   * @return {Promise<string>} Returns a promise that resolves to a newly refreshed token.
+   * @throws {HttpException} Throws an exception if the token is missing or invalid.
+   */
   @HttpCode(HttpStatus.OK)
   @Post('refreshToken')
-  async refreshToken(@Req() request: Request) {
+  async refreshToken(
+    @Req() request: Request,
+  ): Promise<{ refreshToken: string; accessToken: string }> {
     const token = request.headers['authorization']?.split(' ')[1];
     if (!token)
       throw new HttpException('Invalid token', HttpStatus.UNAUTHORIZED);
