004d0f43eeb4772640b58fd0acba883087c70f33
paczaj <marcinpaczkowski@icloud.com>
2025-07-06 07:15:55 +0200

    Integrate ServeStaticModule, ConfigModule, TypeOrmModule, and UserModule into the application module.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,30 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { join } from 'path';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './api/user/user.module';
+import ormConfig from './config/orm.config';
 
 @Module({
-  imports: [],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'frontend', 'browser'),
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [ormConfig],
+      expandVariables: true,
+      envFilePath:
+        process.env.NODE_ENV !== 'production'
+          ? `${process.env.NODE_ENV}.env`
+          : '.env',
+    }),
+    TypeOrmModule.forRoot(ormConfig()),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
