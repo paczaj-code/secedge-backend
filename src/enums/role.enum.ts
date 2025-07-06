6f884cd71b6097f4f3a0a6279a7864442910578c
paczaj <marcinpaczkowski@icloud.com>
2025-07-06 07:14:59 +0200

    Define Role enum and UserRoles type.

---
+++ src/enums/role.enum.ts
@@ -0,0 +1,7 @@
+export enum Role {
+  OFFICER = 1,
+  SHIFT_SUPERVISOR = 2,
+  TEAM_LEADER = 3,
+  ADMIN = 4,
+  SUPER_ADMIN,
+}
---
+++ src/enums/userRoles.ts
@@ -0,0 +1,7 @@
+export type UserRoles =
+  | 'VIEWER'
+  | 'OFFICER'
+  | 'SHIFT_SUPERVISOR'
+  | 'TEAM_LEADER'
+  | 'ADMIN'
+  | 'SUPER_ADMIN';
