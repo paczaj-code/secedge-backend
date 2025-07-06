64b515b0cb79db3e47bd207210df031806f560c8
paczaj <marcinpaczkowski@icloud.com>
2025-07-06 12:20:38 +0200

    Add detailed JSDoc comments to Role decorator for improved documentation and clarity

--- src/decorators/role.decorator.ts
+++ src/decorators/role.decorator.ts
@@ -1,4 +1,17 @@
 import { SetMetadata } from '@nestjs/common';
 
+/**
+ * A constant holding the key used to represent roles in a data object or system.
+ * This key can be used to fetch, store, or manage assigned roles in an application.
+ */
+
 export const ROLE_KEY = 'roles';
-export const Role = (...role: string[]) => SetMetadata(ROLE_KEY, role);
+/**
+ * A decorator function used to assign roles to a particular method or class.
+ * This function utilizes the SetMetadata function to attach metadata for role-based access control.
+ *
+ * @param {...string} role - A list of roles to be assigned. Accepts multiple string arguments representing the roles.
+ * @returns {Function} - A decorator function that stores the roles in metadata to be used by the application.
+ */
+export const Role = (...role: string[]): Function =>
+  SetMetadata(ROLE_KEY, role);
