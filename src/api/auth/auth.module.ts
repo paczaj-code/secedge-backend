1aa5af7cb4bf1db2fa369487d186a8d9b9cfedd5
paczaj <marcinpaczkowski@icloud.com>
2025-07-06 12:20:21 +0200

    Add JSDoc comments to AuthModule for improved documentation and clarity

--- src/api/auth/auth.module.ts
+++ src/api/auth/auth.module.ts
@@ -6,6 +6,17 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from '../../entities/user.entity';
 import { JwtModule, JwtService } from '@nestjs/jwt';
 
+/**
+ * The AuthModule class is a feature module that provides authentication
+ * and user-related services, controllers, and configurations.
+ * It integrates with TypeORM for database management.
+ *
+ * Configuration includes:
+ * - Controllers: Handles incoming requests and returns appropriate responses.
+ * - Providers: Supplies service classes for implementing authentication logic.
+ * - Imports: Integrates the module with TypeORM to handle database entities.
+ * - Exports: Makes specific providers available for use in other modules.
+ */
 @Module({
   controllers: [AuthController],
   providers: [AuthService, UserService, JwtService],
