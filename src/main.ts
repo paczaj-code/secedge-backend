15515bfa1e406099d105f4f012f19d9af55782b5
paczaj <marcinpaczkowski@icloud.com>
2025-07-10 15:11:22 +0200

    Enable GlobalExceptionFilter in main bootstrap function

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as compression from 'compression';
+import { GlobalExceptionFilter } from './exception/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +12,7 @@ async function bootstrap() {
     origin: '*',
     credentials: true,
   });
+  app.useGlobalFilters(new GlobalExceptionFilter());
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
